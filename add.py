330975563b8524a1f4cbae222a9f02c70f5b3949
Vladislav <vladislav.fedyukov@gmail.com>
2023-07-13 14:54:30 +0300

    изменение

--- add.py
+++ add.py
@@ -2,8 +2,8 @@ import os
 
 def add_dir_with_files(files_list: list) -> list:
     
-    os.mkdir('Homeworks/HW_7/files')
-    os.chdir('Homeworks/HW_7/files')
+    os.mkdir('HW_7/files')
+    os.chdir('HW_7/files')
 
     for item in files_list: 
         with open(item, 'w', encoding='utf-8') as f:
@@ -13,6 +13,6 @@ def add_dir_with_files(files_list: list) -> list:
     return dir_list
 
 if __name__ == '__main__':
-    files_list = ['testqwerty.txt', 'dataqwerty.txt', 'exampleqwerty.pdf']
+    files_list = ['test.txt', 'data.txt', 'example.pdf']
 
     print(add_dir_with_files(files_list))
